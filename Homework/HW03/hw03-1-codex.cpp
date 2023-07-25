92775a81009eed3673f8e9150bd84fee0bb1d77c
Kira101111010110 <137255457+Kira101111010110@users.noreply.github.com>
2023-07-25 18:48:53 +0700

    updatehw03-1

--- Homework/HW03/hw03-1-codex.cpp
+++ Homework/HW03/hw03-1-codex.cpp
@@ -11,5 +11,8 @@ int main() {
           if( i == 8 ) {
             printf( "This is 8. " ) ; 
           }
-        } printf( "ok" ) ;
-    } return 0 ; }
+        }
+        printf( "ok" ) ;
+    } 
+    return 0 ; 
+}
