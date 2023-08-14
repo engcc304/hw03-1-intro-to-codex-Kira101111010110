c64fd7d03264b11f20040c92872b81246f91c5d7
Kira101111010110 <137255457+Kira101111010110@users.noreply.github.com>
2023-08-14 22:29:59 +0700

    updatehw03-1

--- Homework/HW01/hw03-1-codex.cpp
+++ Homework/HW01/hw03-1-codex.cpp
@@ -6,13 +6,15 @@ int main() {
     int a = 10 ;
     int b = 20, c = 30, d = 40 ;
      
-    printf( "This is %d + %d = %d " , a , b , a+b ) ;
+    printf( "This is %d + %d = %d ", a, b, a+b ) ;
     for( int i = 0 ; i < d - c ; i++ ) {
         if( i < c ) {
             printf( "Here " ) ;
             if( i == 8 ) {
                 printf( "This is 8. " ) ; 
-            }
-        } printf( "ok" ) ;
-    } return 0 ; 
-}
+            } //end if
+        } //end if 
+        printf( "ok" ) ;
+    } //end for 
+    return 0 ; 
+} //end main function
